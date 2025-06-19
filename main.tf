1f20aac8bc6c6c49d975c5daea8283551dc254df
Chriss5-2 <christiangiovannixd@gmail.com>
2025-06-19 05:22:03 -0500

    feat(tf): Implementar triggers para la simulación de drift

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 resource "null_resource" "network-1" {
+  triggers = {
+    version = "v1"
+  }
   provisioner "local-exec" {
     command = "echo 'Creando network ${var.name} con puerto ${var.port}'"
   }
